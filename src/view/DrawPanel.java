510582f8af4d90f12837efb6dbc1f537b0f04fd5
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2020-11-11 16:29:09 +0000

    Colour setting for individual shapes

--- src/view/DrawPanel.java
+++ src/view/DrawPanel.java
@@ -6,8 +6,11 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Shape;
 import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.Color;
 import javax.swing.JPanel;
+
+import java.util.Random;
 
 public class DrawPanel extends JPanel {
 
@@ -21,27 +24,36 @@ public class DrawPanel extends JPanel {
     /** Y coordinate to where to draw the line. */
     private int endY;
 
-    /** Current color setting. */
-    private Color colour;
     /** Contains all shapes that are currently painted. */
     private ArrayList<Shape> shapes;
-
     /** Contains undone shapes. */
     private ArrayList<Shape> undoneShapes;
+
+    /** Contains all colours corresponding to the shapes. */
+    private ArrayList<Color> colours;
+    /** Contains colours of undone shapes. */
+    private ArrayList<Color> undoneColours;
 
 
     public DrawPanel() {
         shapes = new ArrayList<Shape>();
         undoneShapes = new ArrayList<Shape>();
+
+        colours = new ArrayList<Color>();
+        undoneColours = new ArrayList<Color>();
     }
 
-    public void paint(Graphics g) {
+    public void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        // todo make this s.t. color only changes for new shapes
-        g2d.setColor(this.colour);
 
-        for(final Shape s : shapes) {
-            g2d.draw(s);
+        for (int i=0; i < shapes.size(); i++) {
+            // Get the shape and colour that go together
+            Shape shape = shapes.get(i);
+            Color colour = colours.get(i);
+
+            // Set colour and draw the shape.
+            g2d.setColor(colour);
+            g2d.draw(shape);
         }
     }
 
@@ -51,8 +63,8 @@ public class DrawPanel extends JPanel {
         this.startY = startY;
         this.endX = endX;
         this.endY = endY;
-        this.colour = colour;
         addLine();
+        addColour(colour);
     }
 
     /** Add a new line to the panel. */
@@ -61,9 +73,23 @@ public class DrawPanel extends JPanel {
         shapes.add(line);
     }
 
+    /** Add the colour of the new shape to the corresponding list.
+     * @param colour The colour to be added. */
+    public void addColour(Color colour) {
+        colours.add(colour);
+    }
+
     public Shape getLastShape(ArrayList<Shape> s) {
         try {
             return s.get(s.size() - 1);
+        } catch(IndexOutOfBoundsException ioobe) {
+            return null;
+        }
+    }
+
+    public Color getLastColour(ArrayList<Color> c) {
+        try {
+            return c.get(c.size() - 1);
         } catch(IndexOutOfBoundsException ioobe) {
             return null;
         }
@@ -77,22 +103,34 @@ public class DrawPanel extends JPanel {
     /** Remove last Shape. */
     public void undo() {
         Shape s = getLastShape(shapes);
-        if (s == null) {
+        Color c = getLastColour(colours);
+        if (s == null & c == null) {
             System.err.println("Nothing to undo");
         } else {
+            // handle shapes
             undoneShapes.add(s);
             shapes.remove(s);
+
+            // handle colours
+            undoneColours.add(c);
+            colours.remove(c);
         }
     }
 
     /** Redo the last removed shape. */
     public void redo() {
         Shape s = getLastShape(undoneShapes);
-        if (s == null) {
+        Color c = getLastColour(colours);
+        if (s == null & c == null) {
             System.err.println("Nothing to redo");
         } else {
+            // handle shape
             undoneShapes.remove(s);
             shapes.add(s);
+
+            // handle colours
+            undoneColours.remove(c);
+            colours.add(c);
         }
     }
 }
